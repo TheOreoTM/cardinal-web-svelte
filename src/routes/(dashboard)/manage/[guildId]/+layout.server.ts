213d478160e94cb38a91fc6ebfc9cccd91932add
TheOreoTM <qwertzybad@gmail.com>
2023-12-13 12:25:19 +0500

    whatever

--- src/routes/(dashboard)/manage/[guildId]/+layout.server.ts
+++ src/routes/(dashboard)/manage/[guildId]/+layout.server.ts
@@ -2,13 +2,11 @@ import { fetchGuild, fetchGuildChannels, fetchGuildRoles } from '$lib/utils/api/
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 import { PathNames } from '$lib/utils/constants';
-import { currentGuild } from '$lib/stores';
 
 export const load = (async ({ params }) => {
 	const guild = await fetchGuild(params.guildId);
 	const channels = await fetchGuildChannels(params.guildId);
 	const roles = await fetchGuildRoles(params.guildId);
-	currentGuild.set(guild);
 	if (!guild) throw redirect(301, PathNames.Manage);
 
 	return {
