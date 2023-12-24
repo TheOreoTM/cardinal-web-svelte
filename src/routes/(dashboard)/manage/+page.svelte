24a0ef96c2c3a2adf4344480f3b1929d717d217a
TheOreoTM <qwertzybad@gmail.com>
2023-12-24 13:20:39 +0500

    a

--- src/routes/(dashboard)/manage/+page.svelte
+++ src/routes/(dashboard)/manage/+page.svelte
@@ -13,20 +13,8 @@
 
 <Meta title="Select a guild" />
 
-<div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-1">
-	<a class="logo-item" href="/">
-		<span>(icon)</span>
-		<span>Skeleton</span>
-	</a>
-	<a class="logo-item" href="/">
-		<span>(icon)</span>
-		<span>Skeleton</span>
-	</a>
-	<!-- ... -->
-</div>
-
-{#if $guilds.values()}
-	<div class="logo-cloud grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-0.5">
+<div class="logo-cloud grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-0.5">
+	{#if $guilds.values()}
 		{#each $guilds.values() as guild}
 			<a class="logo-item" href={`/manage/${guild.id}`}>
 				<div class=" flex flex-col w-full items-center">
@@ -37,30 +25,30 @@
 				</div>
 			</a>
 		{/each}
-	</div>
+	{:else}
+		{'No beans :('}
+	{/if}
+</div>
 
-	<!-- {#if unmutualGuilds}
-			{#each unmutualGuilds as guild}
-				<a
-					class="logo-item card-hover rounded-md relative overflow-hidden"
-					href={generateBotInvite({
-						guildId: guild.id
-					})}
-				>
-					<div class=" flex flex-col w-full items-center">
-						<div class="flex flex-col items-center">
-							<img
-								class="avatar-image w-16 h-full rounded-full"
-								src={getGuildAvatarUrl(guild.id, guild.icon)}
-								use:filter={'#Noir'}
-								alt="guild-icon"
-							/>
-							<span class="mt-2 text-center text-surface-400/50">{guild.name}</span>
-						</div>
-					</div>
-				</a>
-			{/each}
-		{/if} -->
-{:else}
-	{'No beans :('}
-{/if}
+<!-- {#if unmutualGuilds}
+		{#each unmutualGuilds as guild}
+		<a
+		class="logo-item card-hover rounded-md relative overflow-hidden"
+		href={generateBotInvite({
+			guildId: guild.id
+		})}
+		>
+		<div class=" flex flex-col w-full items-center">
+			<div class="flex flex-col items-center">
+				<img
+				class="avatar-image w-16 h-full rounded-full"
+				src={getGuildAvatarUrl(guild.id, guild.icon)}
+				use:filter={'#Noir'}
+				alt="guild-icon"
+				/>
+				<span class="mt-2 text-center text-surface-400/50">{guild.name}</span>
+			</div>
+		</div>
+	</a>
+	{/each}
+	{/if} -->
